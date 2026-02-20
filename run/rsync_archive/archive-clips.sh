8f5f37050c3d49a5012e027da0f121b096bc7e64
Scott <123274976+Scottmg1@users.noreply.github.com>
2026-02-19 18:50:36 -0700

    Update archive-clips.sh

--- run/rsync_archive/archive-clips.sh
+++ run/rsync_archive/archive-clips.sh
@@ -2,7 +2,7 @@
 
 while [ -n "${1+x}" ]
 do
-  if ! (rsync -avhRL --timeout=60 --remove-source-files --no-perms --omit-dir-times \
+  if ! (rsync -avhRL --timeout=600 --remove-source-files --no-perms --omit-dir-times \
         --stats --log-file=/tmp/archive-rsync-cmd.log --ignore-missing-args \
         --files-from="$2" "$1" "$RSYNC_USER@$RSYNC_SERVER:$RSYNC_PATH" &> /tmp/rsynclog || [[ "$?" = "24" ]] )
   then
