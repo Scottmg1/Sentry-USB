40465ea830518b02f1126a9f58b47dcf689402c3
Chad <122223207+ChadR23@users.noreply.github.com>
2026-04-01 06:15:39 -0400

    change notifications history

--- run/cifs_archive/copy-music.sh
+++ run/cifs_archive/copy-music.sh
@@ -84,7 +84,7 @@ function do_music_sync {
 
   if [ $NUM_FILES_COPIED -ne 0 ] || [ $NUM_FILES_DELETED -ne 0 ] || [ $NUM_FILES_ERROR -ne 0 ]
   then
-    /root/bin/send-push-message "$NOTIFICATION_TITLE:" "$message" "" archive
+    /root/bin/send-push-message "$NOTIFICATION_TITLE:" "$message" "" music_sync
   else
     log "$message"
   fi
